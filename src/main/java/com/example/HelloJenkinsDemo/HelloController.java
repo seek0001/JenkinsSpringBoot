94176a8084abe9a8d527e207cc67165d41cdcdf2
seek0001 <seekwanglong@gmail.com>
2025-06-17 21:52:12 +0800

    test

--- src/main/java/com/example/HelloJenkinsDemo/HelloController.java
+++ src/main/java/com/example/HelloJenkinsDemo/HelloController.java
@@ -2,7 +2,7 @@ package com.example.HelloJenkinsDemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@restController
+@RestController
 public class HelloController {
 
     @GetMapping("/hello")
