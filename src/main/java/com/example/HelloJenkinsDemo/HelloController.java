d4c7c0eb1347b7aead623c4b35f03a8aa470c79a
seek0001 <seekwanglong@gmail.com>
2025-06-17 21:29:29 +0800

    test

---
+++ src/main/java/com/example/HelloJenkinsDemo/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.HelloJenkinsDemo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@restController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Jenkins!";
+    }
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Application is running!";
+    }
+}
